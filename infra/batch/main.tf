378dd3fc9f51f387e6342910f4d497909a3ab36f
Ernyoke <ernyoke19930115@gmail.com>
2023-03-11 00:55:27 +0200

    Added necessary resources for batch job. Not yet configured properly.

--- infra/batch/main.tf
+++ infra/batch/main.tf
@@ -1,4 +1,7 @@
-data "aws_iam_policy_document" "batch_assume_role" {
+#--------------------
+# Compute Environment
+#--------------------
+data "aws_iam_policy_document" "assume_role" {
   statement {
     effect = "Allow"
 
@@ -11,13 +14,13 @@ data "aws_iam_policy_document" "batch_assume_role" {
   }
 }
 
-resource "aws_iam_role" "aws_batch_service_role" {
-  name               = "aws_batch_service_role"
-  assume_role_policy = data.aws_iam_policy_document.batch_assume_role.json
+resource "aws_iam_role" "service_role" {
+  name               = "${var.module_name}-service-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
-resource "aws_iam_role_policy_attachment" "aws_batch_service_role" {
-  role       = aws_iam_role.aws_batch_service_role.name
+resource "aws_iam_role_policy_attachment" "service_role_attachment" {
+  role       = aws_iam_role.service_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBatchServiceRole"
 }
 
@@ -31,7 +34,7 @@ resource "aws_subnet" "subnet" {
 }
 
 resource "aws_security_group" "sg" {
-  name = "aws-batch-moves-loader-sg"
+  name = "${var.module_name}-sg"
 
   egress {
     from_port   = 0
@@ -41,11 +44,13 @@ resource "aws_security_group" "sg" {
   }
 }
 
-resource "aws_batch_compute_environment" "sample" {
-  compute_environment_name = "movies-loader"
+resource "aws_batch_compute_environment" "compute_environment" {
+  compute_environment_name = var.module_name
 
   compute_resources {
-    max_vcpus = 16
+    max_vcpus     = 4
+    min_vcpus     = 1
+    desired_vcpus = 2
 
     security_group_ids = [
       aws_security_group.sg.id
@@ -58,7 +63,75 @@ resource "aws_batch_compute_environment" "sample" {
     type = "FARGATE"
   }
 
-  service_role = aws_iam_role.aws_batch_service_role.arn
+  service_role = aws_iam_role.service_role.arn
   type         = "MANAGED"
-  depends_on   = [aws_iam_role_policy_attachment.aws_batch_service_role]
+  depends_on   = [aws_iam_role_policy_attachment.service_role_attachment]
+}
+
+#--------------------
+# Job Queue
+#--------------------
+
+resource "aws_batch_job_queue" "job_queue" {
+  name     = "${var.module_name}-job-queue"
+  state    = "ENABLED"
+  priority = 1
+  compute_environments = [
+    aws_batch_compute_environment.compute_environment.arn,
+  ]
+}
+
+#--------------------
+# Job Definition
+#--------------------
+resource "aws_batch_job_definition" "job_definition" {
+  name = "${var.module_name}-job-definition"
+  type = "container"
+
+  platform_capabilities = [
+    "FARGATE",
+  ]
+
+  container_properties = jsonencode({
+    command = ["echo", "test"]
+    image   = "busybox"
+
+    fargatePlatformConfiguration = {
+      platformVersion = "LATEST"
+    }
+
+    resourceRequirements = [
+      {
+        type  = "VCPU"
+        value = "1"
+      },
+      {
+        type  = "MEMORY"
+        value = "1024"
+      }
+    ]
+
+    executionRoleArn = aws_iam_role.ecs_task_execution_role.arn
+  })
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name               = "${var.module_name}-ecs-task-execution-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
---
+++ infra/batch/variables.tf
@@ -0,0 +1,7 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "module_name" {
+  default = "movies-load-batch"
+}
