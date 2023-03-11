7dbc136e9f921dbf0da28c748f8b2c09c222336f
Ervin Szilagyi <ervin_szilagyi@outlook.com>
2023-03-12 00:38:18 +0200

    Use private VPC with endpoints.

--- infra/batch/vpc.tf
+++ infra/batch/vpc.tf
@@ -4,15 +4,9 @@ resource "aws_vpc" "vpc" {
   tags = {
     "Name" = "${var.module_name}-vpc"
   }
-}
 
-resource "aws_subnet" "public_subnet" {
-  vpc_id     = aws_vpc.vpc.id
-  cidr_block = "10.1.1.0/24"
-
-  tags = {
-    "Name" = "${var.module_name}-public-subnet"
-  }
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 }
 
 resource "aws_subnet" "private_subnet" {
@@ -24,61 +18,98 @@ resource "aws_subnet" "private_subnet" {
   }
 }
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.vpc.id
-
-  tags = {
-    Name = "${var.module_name}-igw"
-  }
+data "aws_vpc_endpoint_service" "s3" {
+  service      = "s3"
+  service_type = "Gateway"
 }
 
-resource "aws_eip" "eip" {
-  vpc = true
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id          = aws_vpc.vpc.id
+  service_name    = data.aws_vpc_endpoint_service.s3.service_name
+  route_table_ids = [aws_route_table.private_rt.id]
+  policy          = <<POLICY
+{
+    "Version": "2008-10-17",
+    "Statement": [
+        {
+            "Action": "*",
+            "Effect": "Allow",
+            "Resource": "*",
+            "Principal": "*"
+        }
+    ]
+}
+POLICY
 }
 
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.eip.id
-  subnet_id     = aws_subnet.public_subnet.id
-
-  tags = {
-    Name = "${var.module_name}-nat"
-  }
-
-  depends_on = [aws_internet_gateway.igw]
+data "aws_vpc_endpoint_service" "dynamodb" {
+  service      = "dynamodb"
+  service_type = "Gateway"
 }
 
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.vpc.id
+resource "aws_vpc_endpoint" "dynamodb" {
+  vpc_id          = aws_vpc.vpc.id
+  service_name    = data.aws_vpc_endpoint_service.dynamodb.service_name
+  route_table_ids = [aws_route_table.private_rt.id]
+  policy          = <<POLICY
+{
+    "Version": "2008-10-17",
+    "Statement": [
+        {
+            "Action": "*",
+            "Effect": "Allow",
+            "Resource": "*",
+            "Principal": "*"
+        }
+    ]
+}
+POLICY
+}
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
+resource "aws_vpc_endpoint" "endpoints" {
+  for_each = {
+    "ecr" : "com.amazonaws.${var.aws_region}.ecr.dkr",
+    "ecr-api" : "com.amazonaws.${var.aws_region}.ecr.api"
+    "logs" : "com.amazonaws.${var.aws_region}.logs"
   }
 
-  tags = {
-    Name = "${var.module_name}-public-rt"
-  }
+  service_name        = each.value
+  vpc_id              = aws_vpc.vpc.id
+  subnet_ids          = [aws_subnet.private_subnet.id]
+  private_dns_enabled = true
+  security_group_ids  = [aws_security_group.tls.id]
+  vpc_endpoint_type   = "Interface"
 }
 
 resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat.id
-  }
 
   tags = {
     Name = "${var.module_name}-private-rt"
   }
 }
 
-resource "aws_route_table_association" "public_rt_association" {
-  subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.public_rt.id
-}
-
 resource "aws_route_table_association" "private_rt_association" {
   subnet_id      = aws_subnet.private_subnet.id
   route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_security_group" "tls" {
+  name        = "${var.module_name}-endpoint-sg"
+  description = "Allow TLS for endpoints."
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.vpc.cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
